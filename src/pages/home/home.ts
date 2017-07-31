ade5bf72399263db7767075b81dbd74ecc40f8ef
Victor Henry <victory21th@gmail.com>
2017-07-31 11:48:06 -0700

    update home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {LoadingController, Loading} from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
@@ -7,8 +9,57 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController) {
+    loading: Loading;
+    offices: any;
 
+    constructor(public navCtrl: NavController, private db: AngularFireDatabase, private loadingCtrl: LoadingController) {
+        this.offices = [];
+    }
+
+    ionViewDidEnter() {
+        this.loading = this.loadingCtrl.create();
+        this.loading.present();
+
+        let offices = this.db.list('/offices', {
+            preserveSnapshot: true,
+            query: {
+                orderByChild: "floorId"
+            }
+        });
+
+        offices.subscribe(snapshots => {
+
+            this.loading.dismiss();
+            this.offices = [];
+
+            snapshots.forEach(snapshot => {
+                console.log(snapshot.key);
+                console.log(snapshot.val());
+
+                let office = snapshot.val();
+                office['$id'] = snapshot.key;
+                this.offices.push(office);
+            });
+        });
+    }
+
+    public buildingOffices(buildingId) {
+        let offices = [];
+        for (let i = 0; i < this.offices.length; i ++) {
+            if (this.offices[i]['buildingId'] == buildingId) {
+                offices.push(this.offices[i]);
+            }
+        }
+
+        return offices;
+    }
+
+    public viewOffice(office) {
+        this.navCtrl.push('OfficeProfilePage', {officeId: office.$id});
+    }
+
+    public viewBuildingList() {
+        this.navCtrl.push('BuildingListPage');
     }
 
 }
