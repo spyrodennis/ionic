ea94e49d32d9b65fb72da14985411ebc770e439b
Richard Thomas <richardthomasworld@gmail.com>
2017-08-07 00:32:23 -0700

    update notifications page

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {PushServiceProvider} from "../../providers/push-service/push-service"
 import {LoadingController, Loading} from 'ionic-angular';
+import {AuthProvider} from '../../providers/auth/auth';
 
 /**
  * Generated class for the NotificationsPage page.
@@ -17,8 +18,10 @@ import {LoadingController, Loading} from 'ionic-angular';
 export class NotificationsPage {
 
     loading: Loading;
+    notifications: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private pushService: PushServiceProvider, private loadingCtrl: LoadingController) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private pushService: PushServiceProvider, private loadingCtrl: LoadingController, private auth: AuthProvider) {
+        this.notifications = [];
     }
 
     ionViewDidEnter() {
@@ -28,6 +31,26 @@ export class NotificationsPage {
         this.pushService.getNotifiactionList().then(res => {
             this.loading.dismiss();
             console.log(res);
+            let notifications = res['data'];
+            let device_notifications = [];
+            this.notifications = [];
+            for (let i = 0; i < notifications.length; i ++) {
+                let notification = notifications[i];
+                console.log(notification);
+                if (typeof notification.config.tokens == "undefined") continue;
+                if (notification.config.tokens.indexOf(this.auth.deviceToken) >= 0) {
+                    device_notifications.push(notification);
+                }
+            }
+            let notiFlag = {};
+            for (let i = device_notifications.length - 1; i >= 0; i --) {
+                let notification = device_notifications[i];
+                let payload = notification.config.notification.payload;
+                if (typeof notiFlag[payload.typeKey] == "undefined") {
+                    this.notifications.splice(0, 0, notification);
+                    notiFlag[payload.typeKey] = true;
+                }
+            }
         }).catch(err => {
             this.loading.dismiss();
             console.log(err);
@@ -38,4 +61,10 @@ export class NotificationsPage {
         console.log('ionViewDidLoad NotificationsPage');
     }
 
+    viewNotification(notification) {
+        let payload = notification.config.notification.payload;
+        if (payload.type == "request") {
+            this.navCtrl.push('MaintenanceTrackerPage', {requestKey: payload.typeKey});
+        }
+    }
 }
